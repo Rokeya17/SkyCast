108fd111a64cef6b8eadf13bd7d9b8e14cf73b8d
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-05-29 23:24:27 +0600

    Home screen has been added.

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 import 'package:weather_app/utility/images.dart';
+import 'package:weather_app/view/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -9,14 +11,16 @@ class SplashScreen extends StatelessWidget {
   }
 
   Future<void> goToNextScreen() async {
-    Future.delayed(const Duration(seconds: 3)).then((value) {});
+    Future.delayed(const Duration(seconds: 3)).then((value) {
+      Get.offAll(HomeScreen);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Weather app'),
+          title: const Text('Weather app'),
         ),
         body: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
